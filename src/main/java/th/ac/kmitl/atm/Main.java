d184b321ad2598f490d08dbf635535ba997dc26b
61051054 <72656159+61051054@users.noreply.github.com>
2020-10-10 21:12:23 +0700

    Add files via upload

---
+++ src/main/java/th/ac/kmitl/atm/Main.java
@@ -0,0 +1,18 @@
+package th.ac.kmitl.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean.xml");
+        ATM atm = context.getBean(ATM.class);
+
+        AtmUI atmUI = context.getBean(AtmUI.class);
+        atmUI.run();
+
+
+    }
+}
